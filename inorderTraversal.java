959f814f8793776fbfa8e9f0c4ed5f2023ad34b3
nikku-14 <76612416+nikku-14@users.noreply.github.com>
2022-10-24 18:41:14 +0530

    Create inorderTraversal.java

---
+++ inorderTraversal.java
@@ -0,0 +1,12 @@
+class inorder {
+    List<Integer > ans = new LinkedList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root == null)
+            return ans;
+        inorderTraversal(root.left);
+        ans.add(root.val);
+        inorderTraversal(root.right);
+        return ans;
+        
+    }
+}
